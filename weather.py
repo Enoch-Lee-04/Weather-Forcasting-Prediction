6e1982c1c678845de710f7df8c1a41d0395e587c
Enoch-Lee-04 <lee.enoch03@gmail.com>
2025-03-10 20:18:31 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+from prophet import Prophet
+from prophet.plot import plot_plotly, plot_components_plotly
 
 data = pd.read_csv("DailyDelhiClimateTrain.csv")
 print(data.head())
@@ -18,10 +20,10 @@ avg_temp_figure = px.line(data, x = "date",
 avg_temp_figure.show()
 
 # plotting average humidity over the years
-ave_humidity_figure = px.line(data, x = "date",
+avg_humidity_figure = px.line(data, x = "date",
                             y = "humidity",
                             title = "Average Humidity in Delhi Over the Years")
-ave_humidity_figure.show()
+avg_humidity_figure.show()
 
 # plotting average wind speed over the years
 avg_wind_speed_figure = px.line(data, x = "date",
@@ -29,10 +31,33 @@ avg_wind_speed_figure = px.line(data, x = "date",
                                 title = "Average Wind Speed in Delhi Over the Years")
 avg_wind_speed_figure.show()
 
-# plotting relationship between average temperature and average humidity
-temp_humidity = px.scatter(data_frame = data, x = "humidity",
-                                y = "meantemp", size = "meantemp",
-                                trendline = "ols",
-                                title = "Relationship between Average Temperature and Humidity")
-temp_humidity.show()
+# # plotting relationship between average temperature and average humidity
+# temp_humidity = px.scatter(data_frame = data, x = "humidity",
+#                                 y = "meantemp", size = "meantemp",
+#                                 trendline = "ols",
+#                                 title = "Relationship between Average Temperature and Humidity")
+# temp_humidity.show()
 
+# change the data type to datetime
+data['date'] = pd.to_datetime(data['date'], format = '%Y-%m-%d')
+data['year'] = data['date'].dt.year
+data['month'] = data['date'].dt.month
+print(data.head())
+
+# plotting temperature change in Delhi over the years
+plt.style.use('fivethirtyeight')
+plt.figure(figsize = (15, 10))
+plt.title("Temperature Change in Delhi Over the Years")
+sns.lineplot(data = data, x = 'month', y = 'meantemp', hue = 'year')
+plt.show()
+
+# converting the data to Prophet format
+forecast_data = data.rename(columns = {'date': 'ds', 'meantemp': 'y'})
+
+# importing Prophet
+model = Prophet()
+model.fit(forecast_data)
+forecasts = model.make_future_dataframe(periods = 365)
+predictions = model.predict(forecasts)
+fig = plot_plotly(model, predictions)
+fig.show()
